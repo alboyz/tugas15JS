0c13321f77ea53843cd0c6e6b9f62826afae45a6
ali <alboyzskeaters@gmail.com>
2020-01-09 16:45:51 +0700

    Tugas15JS

---
+++ tugas15.js
@@ -0,0 +1,8 @@
+let AB = 8;
+let BC = 6;
+console.log("Panjang AB"+" "+AB);
+console.log("Panjang BC"+" "+BC);
+let c = Math.pow(AB,2)+Math.pow(BC,2)
+console.log("Nilai dari AB kuadrat ditambah BC kuadrat adalah :"+" "+c);
+let AC = Math.sqrt(c)
+console.log("jadi panjang AC adalah : "+AC+"cm");
